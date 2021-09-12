8dbfca539df7a6e3f54d91177a96a6a63a431c7d
chrisbdevelopper <kenshin91cb@gmail.com>
2021-09-12 10:05:28 +0200

    DESKTOP modif form twig help err_msg

--- src/Form/BienImmoType.php
+++ src/Form/BienImmoType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\BienImmo;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,13 +14,38 @@ class BienImmoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('building')
-            ->add('street')
-            ->add('cp')
-            ->add('city')
-            ->add('loyer_hc')
-            ->add('charges')
-            ->add('solde', NumberType::class)
+            ->add('building', TextType::class,[
+                'label' => "Tire du bien",
+                'attr' => [],
+            ])
+            ->add('street', TextType::class,[
+                'label' => "Nom de la rue",
+                'attr' => [],
+            ])
+            ->add('cp', NumberType::class,[
+                'label' => "Code postal",
+                'invalid_message' => 'Valeur incorrecte',
+                'attr' => [],
+            ])
+            ->add('city', TextType::class,[
+                'label' => "Ville",
+                'attr' => [],
+            ])
+            ->add('loyer_hc', NumberType::class,[
+                'label' => "Loyer HC*",
+                'help' => '* Loyer sans les charges',
+                'invalid_message' => 'Valeur incorrecte',
+                'attr' => [],
+            ])
+            ->add('charges', NumberType::class,[
+                'label' => "Charges",
+                'invalid_message' => 'Valeur incorrecte',
+                'attr' => [],
+            ])
+            ->add('solde', NumberType::class, [
+                'label' => "Solde",
+                'invalid_message' => 'Valeur incorrecte',
+            ])
         ;
     }
 
