b274ee72b374ad3923385baa19910e3f5a21576d
K4gur0 <kenshin91cb@gmail.com>
2021-09-16 11:40:03 +0200

    LAPTOP add configuration for swiftmailer

--- src/Controller/ImmoController.php
+++ src/Controller/ImmoController.php
@@ -64,8 +64,10 @@ class ImmoController extends AbstractController
      */
     public function mail(MailController $mailController, QuittanceRepository $quittanceRepository, $id){
         $quittance = $quittanceRepository->find($id);
+        $locataire = $quittance->getLocataire();
         $quittance_file_path = '../assets/files/quittances/' . $quittance . '.docx';
         $mailController->sendMessage($quittance_file_path);
+        $this->addFlash('success', 'La quittance de loyer à bien été envoyer pour : ' . $locataire );
         return $this->redirectToRoute('immo_accueil');
     }
 
