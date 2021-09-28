ed544f2e4a35a8a2b0e9ab59f3236cf21a8129f7
K4gur0 <kenshin91cb@gmail.com>
2021-09-28 15:58:50 +0200

    LAPTOP add entrprise entity

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use App\Repository\EntrepriseRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/entreprenariat", name="entreprenariat_")
+ * @IsGranted("ROLE_USER")
  */
 class JobController extends AbstractController
 {
@@ -20,7 +22,7 @@ class JobController extends AbstractController
         $entreprises = $entrepriseRepository->findAll();
 
         return $this->render('entreprenariat/homepage.html.twig', [
-
+            'entreprises' => $entreprises,
         ]);
     }
 }
