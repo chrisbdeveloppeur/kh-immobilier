ab5979ce8ffe86e2ea34ad254f082966ab6c6478
K4gur0 <kenshin91cb@gmail.com>
2021-10-07 16:10:08 +0200

    LAPTOP optimisation QuittancesController for fill template devis

--- src/Form/BienImmoType.php
+++ src/Form/BienImmoType.php
@@ -9,6 +9,7 @@ use App\Repository\BienImmoRepository;
 use App\Repository\LocataireRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -58,7 +59,7 @@ class BienImmoType extends AbstractType
                 'attr' => [],
             ])
             ->add('loyer_hc', NumberType::class,[
-                'label' => "Loyer HC*",
+                'label' => "Loyer HC",
                 'help' => 'Loyer sans les charges',
                 'invalid_message' => 'Valeur incorrecte',
                 'attr' => [],
@@ -68,6 +69,52 @@ class BienImmoType extends AbstractType
                 'invalid_message' => 'Valeur incorrecte',
                 'attr' => [],
             ])
+            ->add('echeance', NumberType::class,[
+                'label' => "Echéance",
+                'help' => 'Date d\'échéance de payement',
+                'invalid_message' => 'Valeur incorrecte',
+                'attr' => [],
+            ])
+        /*
+            ->add('echeance', ChoiceType::class,[
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                    '6' => 6,
+                    '7' => 7,
+                    '8' => 8,
+                    '9' => 9,
+                    '10' => 10,
+                    '11' => 11,
+                    '12' => 12,
+                    '13' => 13,
+                    '14' => 14,
+                    '15' => 15,
+                    '16' => 16,
+                    '17' => 17,
+                    '18' => 18,
+                    '19' => 19,
+                    '20' => 20,
+                    '21' => 21,
+                    '22' => 22,
+                    '23' => 23,
+                    '24' => 24,
+                    '25' => 25,
+                    '26' => 26,
+                    '27' => 27,
+                    '28' => 28,
+                    '29' => 29,
+                    '30' => 30,
+                ],
+                'label' => "Echéance*",
+                'help' => 'Date d\'échéance de payement',
+                'invalid_message' => 'Valeur incorrecte',
+                'attr' => [],
+            ])
+        */
             ->add('solde', NumberType::class, [
                 'mapped' => false,
                 'label' => "Solde",
