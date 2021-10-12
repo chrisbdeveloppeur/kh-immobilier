17bc717a8e5f37dafb29901b6531f00749c38363
K4gur0 <kenshin91cb@gmail.com>
2021-10-12 11:53:59 +0200

    LAPTOP working frontend

--- src/Form/EntrepriseType.php
+++ src/Form/EntrepriseType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +13,24 @@ class EntrepriseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('siret')
-            ->add('tva_number')
+            ->add('name', TextType::class,[
+                'label' => 'Nom de l\'entreprise',
+                'attr' => ['class' => 'input is-small has-text-centered'],
+                'required' => true,
+            ])
+            ->add('siret', TextType::class,[
+                'label' => 'N° de SIRET',
+                'attr' => ['class' => 'input is-small has-text-centered'],
+                'required' => false,
+            ])
+            ->add('tva_number', TextType::class,[
+                'label' => 'N° TVA',
+                'attr' => ['class' => 'input is-small has-text-centered'],
+                'required' => false,
+            ])
             ->add('imageFile', FileType::class,[
+                'label' => 'Image / Logo',
+                'attr' => ['class' => 'input is-small has-text-centered'],
                 'required' => false,
             ])
         ;
