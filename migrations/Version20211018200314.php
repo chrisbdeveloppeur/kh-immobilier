823c704da0c6398ab5373c53ebdc3aaa0c8727fe
K4gur0 <kenshin91cb@gmail.com>
2021-10-18 22:23:48 +0200

    LAPTOP remove pointer events for quittance form

--- migrations/Version20211018200314.php
+++ migrations/Version20211018200314.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211018104426 extends AbstractMigration
+final class Version20211018200314 extends AbstractMigration
 {
     public function getDescription(): string
     {
