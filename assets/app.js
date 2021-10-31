1fd7395ffe9fde08c8745b4ae52e9a1fd71ba131
chrisbdevelopper <kenshin91cb@gmail.com>
2021-10-31 11:02:09 +0100

    DESKTOP modification front end accueil page

--- assets/app.js
+++ assets/app.js
@@ -9,10 +9,13 @@
 import './styles/app.scss';
 
 const immo_illustration = require('./img/png/immo_illustration.png');
+const immo_illustration_2 = require('./img/svg/undraw_houses-3-_xwf7.svg');
 const entreprenariat_illustration = require('./img/png/job_illustration.png');
+const entreprenariat_illustration_2 = require('./img/svg/undraw_windy_day_x-63-l.svg');
 const devis_illustration = require('./img/png/Files_And_Folder.png');
 const facture_illustration = require('./img/png/facture.png');
 const startup_illustration = require('./img/png/Startup.png');
+
 
 var bulmaCalendar = require('bulma-calendar');
 
