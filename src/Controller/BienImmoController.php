7104f05c108b6e836a179dfd03253a384b78431f
K4gur0 <kenshin91cb@gmail.com>
2021-11-18 16:17:41 +0100

    LAPTOP modif onhover animatoin css

--- src/Controller/BienImmoController.php
+++ src/Controller/BienImmoController.php
@@ -15,12 +15,10 @@ use App\Repository\QuittanceRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use function Sodium\add;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 
