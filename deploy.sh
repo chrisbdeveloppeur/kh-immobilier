95e0cf1ac3e4df69ea16993381960f0e435b55fd
chrisbdevelopper <kenshin91cb@gmail.com>
2021-11-28 18:49:18 +0100

    DESKTOP prepare .env for deploy planethoster

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rsync -avz ./ -e "ssh -p 5022"  weskyknm@node1-ca.n0c.com:~/public_html/edit-word --include=public/build --include=public/bundles --include=public/.htaccess --include=vendor --exclude-from=.gitignore --exclude=".env.local" --exclude="public/images" --exclude="public/documents"
