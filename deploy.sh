31d5e9abb37f844577374080d52bcda8eb15fceb
K4gur0 <kenshin91cb@gmail.com>
2021-11-29 16:50:43 +0100

    LAPTOP add docx button for qiuttances

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -avz ./ -e "ssh -p 5022"  weskyknm@node1-ca.n0c.com:~/public_html/edit-word --include=public/build --include=public/bundles --include=public/.htaccess --include=vendor --include=.env --exclude=".env.local" --exclude="public/images" --exclude="public/documents"
+rsync -avz ./ -e "ssh -p 5022"  weskyknm@node1-ca.n0c.com:~/public_html/edit-word --include=public/build --include=public/bundles --include=public/.htaccess --include=vendor --include=.env --exclude-from=.gitignore --exclude=".*" --exclude=".env.local" --exclude="public/images" --exclude="public/documents"
