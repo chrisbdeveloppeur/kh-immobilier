5aae25708b8331906f9ba28416318244c30ef358
K4gur0 <kenshin91cb@gmail.com>
2022-03-21 15:58:31 +0100

    LAPTOP modif create quittance, erease the duplicated quittance file

--- src/Controller/QuittancesController.php
+++ src/Controller/QuittancesController.php
@@ -214,10 +214,10 @@ class QuittancesController extends AbstractController
 
         if ($quittance->getPayed()){
             $quittance->setPayed(false);
-            $this->addFlash('info', 'Vous venez de définir la quittance <b>'.$quittance->getFileName().'</b> au status "non payée"');
+            //$this->addFlash('info', 'Vous venez de définir la quittance <b>'.$quittance->getFileName().'</b> au status "non payée"');
         }else{
             $quittance->setPayed(true);
-            $this->addFlash('success', 'Vous venez de définir la quittance <b>'.$quittance->getFileName().'</b> au status "payée"');
+            //$this->addFlash('success', 'Vous venez de définir la quittance <b>'.$quittance->getFileName().'</b> au status "payée"');
         }
         $em->flush();
         $referer = $request->headers->get('referer');
