fd505e6f4ff7dd24e09fa345559f9fa4f0e07141
K4gur0 <kenshin91cb@gmail.com>
2022-04-05 00:12:05 +0200

    LAPTOP modif front bien_card

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\BienImmo;
+use App\Entity\Copropriete;
 use App\Entity\Entreprise;
 use App\Entity\Locataire;
 use App\Entity\Solde;
@@ -25,7 +26,7 @@ class AppFixtures extends Fixture
     {
         $faker = Faker\Factory::create('fr_FR');
 
-///*
+/*
         $locataire = $this->setLocataire('Couple','Axel et Laurine','MAKAGNON','axel.makagnon@gmail.com','Espèces');
         $bien = $this->setBien($locataire,'4 Place Vaillant couturier','91100','Corbeil-Essonnes','800','200','7','T4','FONCIA','80');
         $manager->persist($locataire);
@@ -50,6 +51,8 @@ class AppFixtures extends Fixture
         $bien5 = $this->setBien($locataire5,'26 rue Jean piestre','91100','Corbeil-Essonnes','700','50','10','Studio','MATERA','35');
         $manager->persist($locataire5);
         $manager->persist($bien5);
+
+*/
 
         //make standard $users
         for ($i = 1; $i <= 5; $i++){
@@ -83,41 +86,50 @@ class AppFixtures extends Fixture
 
 //*/
 
-//        for ($i = 0; $i<= 15; $i++) {
-//            $locataire = new Locataire();
-//            $sexe = $faker->randomElement(['M.', 'Mme.']);
-//            if ($sexe == 'M.'){
-//                $first_name = $faker->firstNameMale;
-//            }else{
-//                $first_name = $faker->firstNameFemale;
-//            }
-//            $last_name = $faker->lastName;
-//            $mail = $faker->randomElement(['@gmail.com','@yahoo.com','@free.fr']);
-//            $locataire->setFirstName($first_name);
-//            $locataire->setLastName($last_name);
-//            $locataire->setGender($sexe);
-//            $locataire->setMode($faker->randomElement(['Virement banquaire', 'Espèces', 'Chèque']));
-//            $locataire->setEmail(mb_strtolower($first_name)  . '.' . mb_strtolower($last_name) . $mail);
-//
-//            $bien = new BienImmo();
-//            $bien->setBuilding($faker->streetName);
-//            $bien->setStreet($faker->streetAddress);
-//            $bien->setCp($faker->postcode);
-//            $bien->setCity($faker->city);
-//            $bien->setLoyerHc($faker->numberBetween(300,1200));
-//            $bien->setCharges($faker->numberBetween(50,300));
-//            $bien->setEcheance($faker->randomElement([5,10,15]));
-//
-//            //$solde = new Solde();
-//            //$solde->setBienImmo($bien);
-//            //$solde->setMalusQuantity($faker->randomElement([0,50,100,300,500]));
-//
-//            $locataire->setLogement($bien);
-//
-//            //$manager->persist($solde);
-//            $manager->persist($bien);
-//            $manager->persist($locataire);
-//        }
+        for ($i = 0; $i<= 15; $i++) {
+            $locataire = new Locataire();
+            $sexe = $faker->randomElement(['M.', 'Mme.']);
+            if ($sexe == 'M.'){
+                $first_name = $faker->firstNameMale;
+            }else{
+                $first_name = $faker->firstNameFemale;
+            }
+            $last_name = $faker->lastName;
+            $mail = $faker->randomElement(['@gmail.com','@yahoo.com','@free.fr']);
+            $locataire->setFirstName($first_name);
+            $locataire->setLastName($last_name);
+            $locataire->setGender($sexe);
+            $locataire->setMode($faker->randomElement(['Virement banquaire', 'Espèces', 'Chèque']));
+            $locataire->setEmail(mb_strtolower(str_replace(' ', '', $first_name))  . '.' . mb_strtolower(str_replace(' ', '', $last_name)) . $mail);
+            $locataire->setPhone($faker->randomElement([$faker->phoneNumber,'']));
+
+            $bien = new BienImmo();
+            $bien->setBuilding($faker->streetName);
+            $bien->setStreet($faker->streetAddress);
+            $bien->setCp($faker->postcode);
+            $bien->setCity($faker->city);
+            $bien->setLoyerHc($faker->numberBetween(300,1200));
+            $bien->setCharges($faker->numberBetween(50,300));
+            $bien->setEcheance($faker->randomElement([5,10,15]));
+
+            $copropriete = new Copropriete();
+            $copropriete->setName($faker->randomElement(['MATERA','CITYA','FONCIA']));
+            $copropriete->setPhone($faker->randomElement([$faker->phoneNumber]));
+            $copropriete->setEmail($faker->randomElement([$faker->companyEmail]));
+            $copropriete->setAdresse($faker->address);
+            $copropriete->setContact(strtoupper($faker->lastName) .' '. $faker->firstName);
+            $bien->setCopropriete($faker->randomElement([$copropriete,null]));
+
+            //$solde = new Solde();
+            //$solde->setBienImmo($bien);
+            //$solde->setMalusQuantity($faker->randomElement([0,50,100,300,500]));
+
+            $locataire->setLogement($bien);
+
+            //$manager->persist($solde);
+            $manager->persist($bien);
+            $manager->persist($locataire);
+        }
 
         for ($j = 0; $j<=2 ; $j++){
             $entreprise = new Entreprise();
