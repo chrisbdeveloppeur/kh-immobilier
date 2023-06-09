f21256896533ef7c5d96b56b48f60c146ee93d15
K4gur0 <kenshin91cb@gmail.com>
2023-06-09 16:05:23 -0400

    LAPTOP
    DEPLOY PRO - ajout du fichier .htacess configuré

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -avz ./ -e "ssh -p 5022"  ddkjywcr@node171-eu.n0c.com:~/public_html/kh-immobilier --include=public/build --include=public/bundles --include=public/.htaccess --include=.env --exclude=vendor --exclude-from=.gitignore --exclude=".*" --exclude=".env.local" --exclude="public/images" --exclude="public/documents" --exclude=MailHog_windows_386.exe --exclude=deploy.sh
+rsync -avz ./ -e "ssh -p 5022"  ddkjywcr@node171-eu.n0c.com:~/public_html/kh-immobilier --include=public/build --include=public/bundles --include=public/.htaccess --include=.env --exclude=".env.local" --exclude="public/images" --exclude="public/documents" --exclude=MailHog_windows_386.exe --exclude=deploy.sh
