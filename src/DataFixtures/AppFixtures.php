c61d3d54699b32dc4ae57f56d13e0e2df06186e2
K4gur0 <kenshin91cb@gmail.com>
2023-07-13 22:54:33 -0400

    LAPTOP
    MAJ - Modification du block status des loyer annuel.
    FIXTURES - Mise a jour des datasFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,9 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\BienImmo;
 use App\Entity\Copropriete;
-use App\Entity\Entreprise;
 use App\Entity\Locataire;
-use App\Entity\Solde;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -54,12 +52,8 @@ class AppFixtures extends Fixture
 
 */
 
-        //make standard $users
-        for ($i = 1; $i <= 5; $i++){
-
-        }
-
         //make privilege $users
+        /*
         for ($i = 1; $i <= 2; $i++){
             $user = new User();
             $user->setEmail('admin'.$i.'@gmail.com');
@@ -73,114 +67,97 @@ class AppFixtures extends Fixture
 
             $manager->persist($user);
         }
-
+        */
 //*/
 
         for ($i = 0; $i<= 15; $i++) {
-            $locataire = new Locataire();
-            $sexe = $faker->randomElement(['M.', 'Mme.']);
-            if ($sexe == 'M.'){
-                $first_name = $faker->firstNameMale;
-            }else{
-                $first_name = $faker->firstNameFemale;
-            }
-            $last_name = $faker->lastName;
-            $mail = $faker->randomElement(['@gmail.com','@yahoo.com','@free.fr']);
-            $locataire->setFirstName($first_name);
-            $locataire->setLastName($last_name);
-            $locataire->setGender($sexe);
-            $locataire->setMode($faker->randomElement(['Virement banquaire', 'Espèces', 'Chèque']));
-            $locataire->setEmail(mb_strtolower(str_replace(' ', '', $first_name))  . '.' . mb_strtolower(str_replace(' ', '', $last_name)) . $mail);
-            $locataire->setPhone($faker->randomElement([$faker->phoneNumber,'']));
-
-            $bien = new BienImmo();
-            $bien->setBuilding($faker->streetName);
-            $bien->setStreet($faker->streetAddress);
-            $bien->setCp($faker->postcode);
-            $bien->setCity($faker->city);
-            $bien->setLoyerHc($faker->numberBetween(300,1200));
-            $bien->setCharges($faker->numberBetween(50,300));
-            $bien->setEcheance($faker->randomElement([5,10,15]));
-
-            $copropriete = new Copropriete();
-            $copropriete->setName($faker->randomElement(['MATERA','CITYA','FONCIA']));
-            $copropriete->setPhone($faker->randomElement([$faker->phoneNumber]));
-            $copropriete->setEmail($faker->randomElement([$faker->companyEmail]));
-            $copropriete->setAdresse($faker->address);
-            $copropriete->setContact(strtoupper($faker->lastName) .' '. $faker->firstName);
-            $bien->setCopropriete($faker->randomElement([$copropriete,null]));
             $user = $this->setUser($i);
-            $bien->setUser($user);
-            //$solde = new Solde();
-            //$solde->setBienImmo($bien);
-            //$solde->setMalusQuantity($faker->randomElement([0,50,100,300,500]));
 
-            $locataire->setLogement($bien);
-            $locataire->setUser($user);
+            for ($j = 0; $j <= 5; $j++)
+            {
+                $locataire = $this->setLocataire();
+                $bien = $this->setBien();
+                $bien->addLocataire($locataire);
+                $user->addLocataire($locataire);
+                $user->addBiensImmo($bien);
+            }
 
-            //$manager->persist($solde);
-            $manager->persist($user);
-            $manager->persist($bien);
             $manager->persist($locataire);
+            $manager->persist($bien);
+            $manager->persist($user);
         }
-
-//        for ($j = 0; $j<=2 ; $j++){
-//            $entreprise = new Entreprise();
-//            $name = '';
-//            switch ($j){
-//                case 0:
-//                    $name = 'chrisBdev';
-//                    break;
-//                case 1:
-//                    $name = 'Steadiness';
-//                    break;
-//                case 2:
-//                    $name = 'Kingdom Immobilier';
-//                    break;
-//            }
-//            $entreprise->setName($name);
-//            $manager->persist($entreprise);
-//        }
 
         $manager->flush();
     }
 
 
-    private function setLocataire($sexe, $first_name, $last_name, $mail, $mode): Locataire
+    private function setLocataire(): Locataire
     {
-        $locataire = new Locataire();
+        $faker = Faker\Factory::create('fr_FR');
 
+        $locataire = new Locataire();
+        $sexe = $faker->randomElement(['M.', 'Mme.']);
+        if ($sexe == 'M.'){
+            $first_name = $faker->firstNameMale;
+        }else{
+            $first_name = $faker->firstNameFemale;
+        }
+        $last_name = $faker->lastName;
+        $mail = $faker->randomElement(['@gmail.com','@yahoo.com','@free.fr']);
+        $mode = $faker->randomElement(['Virement banquaire', 'Espèces', 'Chèque']);
         $locataire->setFirstName($first_name);
         $locataire->setLastName($last_name);
         $locataire->setGender($sexe);
         $locataire->setMode($mode);
-        $locataire->setEmail($mail);
+        $locataire->setEmail(mb_strtolower(str_replace(' ', '', $first_name))  . '.' . mb_strtolower(str_replace(' ', '', $last_name)) . $mail);
+        $locataire->setPhone($faker->randomElement([$faker->phoneNumber,'']));
         return $locataire;
     }
 
-    private function setBien($locataire, $street, $cp, $city, $loyer_hc, $charges, $echeance, $type, $copropriete, $superficie): BienImmo
+    private function setBien(): BienImmo
     {
+        $faker = Faker\Factory::create('fr_FR');
 
         $bien = new BienImmo();
 
-        $bien->setStreet($street);
-        $bien->setCp($cp);
-        $bien->setCity($city);
-        $bien->setLoyerHc($loyer_hc);
-        $bien->setCharges($charges);
-        $bien->setEcheance($echeance);
-        $bien->setType($type);
-        $bien->setSuperficie($superficie);
+        $bien->setStreet($faker->streetAddress);
+        $bien->setCp($faker->postcode);
+        $bien->setCity($faker->city);
+        $bien->setBuilding($faker->streetName);
+        $bien->setLoyerHc($faker->numberBetween(300,1200));
+        $bien->setCharges($faker->numberBetween(50,300));
+        $bien->setEcheance($faker->randomElement([5,10,15]));
+        $bien->setType($faker->randomElement([
+            'Chambre',
+            'Studio',
+            'T1',
+            'T2',
+            'T3',
+            'T4',
+            'T5',
+            'T6',
+            ]
+        ));
+        $bien->setSuperficie($faker->randomElement([5,10,15]));
+
+        $copropriete = new Copropriete();
+        $copropriete->setName($faker->randomElement(['MATERA','CITYA','FONCIA']));
+        $copropriete->setPhone($faker->randomElement([$faker->phoneNumber]));
+        $copropriete->setEmail($faker->randomElement([$faker->companyEmail]));
+        $copropriete->setAdresse($faker->address);
+        $copropriete->setContact(strtoupper($faker->lastName) .' '. $faker->firstName);
         $bien->getCopropriete()->setName($copropriete);
-        $locataire->setLogement($bien);
+
         return $bien;
     }
 
     private function setUser($i): User
     {
+        $faker = Faker\Factory::create('fr_FR');
+
         $user = new User();
         $user->setEmail('user'.$i.'@gmail.com');
-//            $user->setGender($faker->randomElement(['M.','Mme.']));
+        $user->setGender($faker->randomElement(['M.','Mme.']));
         $user->setRoles(['ROLE_PROPRIETAIRE']);
         $user->setIsVerified(1);
         $password = $this->encoder->encodePassword($user, '123456');
